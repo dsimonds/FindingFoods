e6f730c9868884e4757edcba48d48f41972a0a53
dsimonds <simonds.derek@gmail.com>
2018-07-27 21:02:41 -0400

    creates base app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FindingFoods';
+    constructor(private http: Http) {}
+
+    itemName: string = '';
+    zip: string = '';
+    response: any;
+    returnedItemName: string = '';
+    itemList = [];
+
+    // search walmart api for items
+    searchItem () {
+        this.http.get('http://api.walmartlabs.com/v1/search?format=json&apiKey=cdbxet7ud8j6ad3eyukndug7&query=' + this.itemName)
+        .subscribe (
+            (response) => {
+                this.response = response;
+                console.log(this.response.json());
+                console.log(this.response.json().items.length);
+
+                for (let i=0; i<this.response.json().items.length; i++) {
+                    this.itemList[i] = this.response.json().items[i].name;
+                }
+
+            }
+        )
+    }
+
+
+
+    title = 'FindingFoods';
 }
